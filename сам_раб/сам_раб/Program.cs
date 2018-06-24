71e8f11dc5ff3c2b672d6867e721a20585374aae
Marina <marinasulamanidze@mail.ru>
2018-06-24 10:56:56 +0300

    Создали проект

---
+++ сам_раб/сам_раб/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace сам_раб
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 5;
+
+        }
+    }
+}
